53d6c785b6d492563310cefc624cb627ea8610a3
EricDevelops <1530714+theEricDevelops@users.noreply.github.com>
2024-05-04 18:12:00 +0000

    Update .gitignore and index.html to show locations on map

---
+++ index.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let app = express();
+let ejs = require('ejs');
+//const haikus = require('./haikus.json');
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('view cache', false);
+app.use(express.static('/', {
+  etag: false,
+  lastModified: false,
+  setHeaders: function (res, path) {
+    res.setHeader('Cache-Control', 'no-store')
+  }
+}))
+// Add the products/ folder to the static middleware
+app.use('/products', express.static('products'));
+
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port);
